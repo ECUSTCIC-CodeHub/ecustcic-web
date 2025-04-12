05135c684483d6daf03b58d6f3f227703efff08d
bestzyq <21013965@mail.ecust.edu.cn>
2025-04-13 00:51:55 +0800

    更新qq群

--- src/data/qqGroups.js
+++ src/data/qqGroups.js
@@ -26,54 +26,54 @@ export const baseGroups = [
     {
       group_id: 626180600,
       category: 'game', 
-      description: '守望先锋'
+      description: '守望先锋/Overwatch'
     },
     {
       group_id: 858930629,
       category: 'game', 
-      description: '卡拉彼丘',
+      description: '卡拉彼丘/Karapichu',
       url: 'https://qm.qq.com/q/wali8w3iV2'
     },
     {
       group_id: 477120987,
       category: 'game', 
-      description: '绝区零',
+      description: '绝区零/ZZZ',
       url: 'https://qm.qq.com/q/92rVXtfQg8'
     },
     {
       group_id: 312064523,
       category: 'game', 
-      description: 'CSGO',
+      description: '反恐精英：全球攻势/CS:GO',
       url: 'https://qm.qq.com/q/QxPdnxOPgO'
     },
     {
       group_id: 98327207,
       category: 'game', 
-      description: '日本麻将',
+      description: '日本麻将/Japanese Mahjong',
       url: 'https://qm.qq.com/q/hfNwJ1SUk8'
     },
     {
       group_id: 795053381,
       category: 'game', 
-      description: '金铲铲之战/云顶之弈',
+      description: '金铲铲之战/云顶之弈/Teamfight Tactics',
       url: 'https://qm.qq.com/q/ciqGSTLoX0'
     },
     {
       group_id: 537333957,
       category: 'game', 
-      description: '重返未来:1999',
+      description: '重返未来:1999/Reverse: 1999',
       url: 'https://qm.qq.com/q/MpJEDEsCmm'
     },
     {
       group_id: 619091353,
       category: 'game', 
-      description: 'APEX',
+      description: 'APEX英雄/Apex Legends',
       url: 'https://qm.qq.com/q/N4fnrAF52s'
     },
     {
       group_id: 529565361,
       category: 'game', 
-      description: '术力口•pjsk',
+      description: '世界计划 多彩舞台！/Project SEKAI',
       url: 'https://qm.qq.com/q/egBTXKKHiE'
     },
     {
@@ -91,13 +91,13 @@ export const baseGroups = [
     {
       group_id: 832114258,
       category: 'game', 
-      description: 'bang dream/邦',
+      description: 'BanG Dream!/邦邦',
       url: 'https://qm.qq.com/q/Q5cFhk3lK2'
     },
     {
       group_id: 555293784,
       category: 'game', 
-      description: 'fgo/命运-冠位指定/Fate/Grand Order',
+      description: '命运-冠位指定/Fate/Grand Order/FGO',
       url: 'https://qm.qq.com/q/2ECTtUmEza'
     },
     {
@@ -105,6 +105,91 @@ export const baseGroups = [
       category: 'game', 
       description: '永劫无间/Naraka: Bladepoint',
       url: 'https://qm.qq.com/q/fujUT0KsRa'
+    },
+    {
+      group_id: 926265440,
+      category: 'game', 
+      description: '英勇之地/Brawl Stars'
+    },
+    {
+      group_id: 552750103,
+      category: 'game', 
+      description: '三角洲行动/Delta Force'
+    },
+    {
+      group_id: 343683662,
+      category: 'game', 
+      description: '绝地求生/PUBG/PlayerUnknown\'s Battlegrounds'
+    },
+    {
+      group_id: 659946675,
+      category: 'game', 
+      description: '最终幻想14/Final Fantasy XIV/FF14'
+    },
+    {
+      group_id: 581341861,
+      category: 'game', 
+      description: '星际争霸/StarCraft'
+    },
+    {
+      group_id: 547342217,
+      category: 'game', 
+      description: 'DOTA2/Defense of the Ancients 2'
+    },
+    {
+      group_id: 658783577,
+      category: 'game', 
+      description: '炉石传说新群/Hearthstone'
+    },
+    {
+      group_id: 372511770,
+      category: 'game', 
+      description: '炉石传说老群/Hearthstone'
+    },
+    {
+      group_id: 626314374,
+      category: 'game', 
+      description: '王者荣耀/Honor of Kings'
+    },
+    {
+      group_id: 104049397,
+      category: 'game', 
+      description: '魔兽争霸3/Warcraft III'
+    },
+    {
+      group_id: 563277221,
+      category: 'game', 
+      description: '穿越火线手游/CrossFire Mobile/CFM'
+    },
+    {
+      group_id: 115950211,
+      category: 'game', 
+      description: '彩虹六号：围攻/Rainbow Six Siege'
+    },
+    {
+      group_id: 941699421,
+      category: 'game', 
+      description: '命运2/Destiny 2'
+    },
+    {
+      group_id: 764552791,
+      category: 'game', 
+      description: '战地风云/Battlefield'
+    },
+    {
+      group_id: 826542331,
+      category: 'game', 
+      description: '死锁/DEADLOCK'
+    },
+    {
+      group_id: 604345565,
+      category: 'game', 
+      description: '英雄联盟手游/League of Legends: Wild Rift'
+    },
+    {
+      group_id: 862900842,
+      category: 'game', 
+      description: '使命召唤/Call of Duty'
     },
     // {
     //   group_id: ,
